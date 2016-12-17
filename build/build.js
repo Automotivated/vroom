d2ac851b7391999cdcb8892f877192175718a6d0
Ferry Kobus <ferry@webmonkeys.nl>
2016-12-17 23:15:11 +0100

    Changed some configs

--- build/build.js
+++ build/build.js
@@ -21,9 +21,7 @@ spinner.start()
 var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
 rm('-rf', assetsPath)
 mkdir('-p', assetsPath)
-cp('-R', 'static/*', assetsPath)
-
-console.log(webpackConfig.module.loaders)
+// cp('-R', 'static/*', assetsPath)
 
 webpack(webpackConfig, function (err, stats) {
   spinner.stop()
