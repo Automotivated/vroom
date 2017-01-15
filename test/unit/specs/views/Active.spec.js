dfbf6f689768d7c1206ab20f42acf55ccf9a074a
Ferry Kobus <ferry@webmonkeys.nl>
2017-01-15 15:27:06 +0100

    Fixed test for active filters

---
+++ test/unit/specs/views/Active.spec.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+import Active from 'src/views/Active'
+import { body } from 'src/store/modules/filters/masterdata/body.js'
+
+describe('Active.vue', () => {
+	it('should render correct contents', () => {
+		const vm = new Vue({
+			el: document.createElement('div'),
+			render: (h) => h(Active),
+			store: new Vuex.Store({
+				modules: {
+					filters: {
+						namespaced: true,
+						state: {
+							filters: {
+								[body.key]: body
+							},
+						},
+						getters: {
+							filteredActiveFilters: () => {
+								return [{
+									key: 'body',
+									value: 'hatchback'
+								}]
+							}
+						}
+					}
+				}
+			})
+		})
+		expect(vm.$el.querySelector('.vrm-active-filters').childNodes.length).to.equal(1)
+	})
+})
