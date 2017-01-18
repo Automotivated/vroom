19e184edb7e9744b7e76d08eeaaf3a3b1978abca
Ferry Kobus <kobus@justlease.nl>
2017-01-18 22:23:23 +0100

    Start refactoring tests

--- test/unit/specs/Vroom.spec.js
+++ test/unit/specs/Vroom.spec.js
@@ -8,12 +8,17 @@ import Vroom from 'src/Vroom'
 import store from 'src/store/store'
 
 describe('Vroom.vue', () => {
-	it('should create a Vroom div', () => {
-		const vm = new Vue({
+	let vm = null
+
+	beforeEach(() => {
+		vm = new Vue({
 			el: document.createElement('div'),
 			store,
 			render: (h) => h(Vroom)
 		})
+	})
+
+	it('should create a Vroom div', () => {
 		expect(vm.$el.classList[0]).to.equal('amv-vrm')
 	})
 })
---
+++ test/unit/specs/views/Loader.spec.js
@@ -0,0 +1,37 @@
+// Import Vue and the component being tested
+import Vue from 'vue'
+import Vuex from 'vuex'
+import Loader from 'src/views/Loader'
+import AppStore from 'src/store/modules/app/app'
+
+Vue.use(Vuex)
+
+describe('Loader', () => {
+	let vm = null
+
+	beforeEach(() => {
+		vm = new Vue({
+			el: document.createElement('div'),
+			store: new Vuex.Store({
+				modules: {
+					app: {
+						namespaced: true,
+						state: {
+							loader: {
+								loading: false,
+								delays: [800]
+							}
+						},
+						getters: AppStore.getters
+					}
+				}
+			}),
+			render: (h) => h(Loader)
+		})
+	})
+
+	it('sets the correct default data', () => {
+		expect(vm.$store.getters['app/loading']).to.equal(false)
+		expect(vm.$store.getters['app/loadingDelay']).to.equal(800)
+	})
+})
